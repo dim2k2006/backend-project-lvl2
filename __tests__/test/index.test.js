6d5c6f2428dc987dd879237355d51390457212c3
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-10-04 19:13:48 +0300

    Add genDiff for flat json

---
+++ __tests__/test/index.test.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import fs from 'fs';
+import genDiff from '../../src';
+
+const fixturesPath = path.join('.', '__fixtures__');
+
+const cases = [
+  [
+    path.join(fixturesPath, 'flat1.json'),
+    path.join(fixturesPath, 'flat2.json'),
+    fs.readFileSync(path.join(fixturesPath, 'flat-expected.txt'), 'utf-8'),
+  ],
+];
+
+describe('Should compare files', () => {
+  test.each(cases)(
+    'Filepath1: %p, Filepath2: %p',
+    (filepath1, filepath2, expected) => {
+      expect(genDiff(filepath1, filepath2)).toEqual(expected);
+    },
+  );
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,98 @@
-import { fileURLToPath } from 'url';
-import path, { dirname as dir } from 'path';
+import path from 'path';
+import fs from 'fs';
+import keys from 'lodash/keys';
+import union from 'lodash/union';
+import has from 'lodash/has';
+import find from 'lodash/find';
+import head from 'lodash/head';
+import tail from 'lodash/tail';
+
+const placeholder = ' ';
+const indent = 2;
+
+const nodeTypes = {
+  ADDED: (key, value) => [`+ ${key}: ${value}`],
+  REMOVED: (key, value) => [`- ${key}: ${value}`],
+  CHANGED: (key, value, prevValue) => [`- ${key}: ${prevValue}`, `+ ${key}: ${value}`],
+  UNCHANGED: (key, value) => [`  ${key}: ${value}`],
+};
+
+const getDiff = (ast) => {
+  const iter = (tree, acc, depth = 1) => {
+    if (!tree.length) return acc.join('\n');
+
+    const node = head(tree);
+
+    const process = nodeTypes[node.type];
+    const result = process(node.key, node.value, node.prevValue)
+      .map((item) => `${placeholder.repeat(indent * depth)}${item}`);
+
+    return iter(tail(tree), [...acc, ...result], depth);
+  };
+
+  const result = iter(ast, []);
+
+  return `{\n${result}\n}`;
+};
+
+const diffs = [
+  {
+    checker: (data1, data2, key) => !has(data1, key) && has(data2, key),
+    process: (data1, data2, key) => ({
+      type: 'ADDED',
+      key,
+      value: data2[key],
+    }),
+  },
+  {
+    checker: (data1, data2, key) => has(data1, key) && !has(data2, key),
+    process: (data1, data2, key) => ({
+      type: 'REMOVED',
+      key,
+      value: data1[key],
+    }),
+  },
+  {
+    checker: (data1, data2, key) => has(data1, key) && has(data2, key) && data1[key] !== data2[key],
+    process: (data1, data2, key) => ({
+      type: 'CHANGED',
+      key,
+      prevValue: data1[key],
+      value: data2[key],
+    }),
+  },
+  {
+    checker: (data1, data2, key) => has(data1, key) && has(data2, key) && data1[key] === data2[key],
+    process: (data1, data2, key) => ({
+      type: 'UNCHANGED',
+      key,
+      value: data1[key],
+    }),
+  },
+];
+
+const buildAst = (data1, data2) => union(keys(data1), keys(data2))
+  .sort()
+  .map((key) => {
+    const { process } = find(diffs, ({ checker }) => checker(data1, data2, key));
+
+    const result = process(data1, data2, key);
+
+    return result;
+  });
 
 const genDiff = (filepath1, filepath2) => {
-  const filename = fileURLToPath(import.meta.url);
-  const dirname = dir(filename);
+  const path1 = path.resolve(filepath1);
+  const path2 = path.resolve(filepath2);
 
-  const path1 = path.resolve(dirname, filepath1);
-  const path2 = path.resolve(dirname, filepath2);
+  const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
 
+  const ast = buildAst(data1, data2);
 
+  const diff = getDiff(ast);
+
+  return diff;
 };
 
 export default genDiff;
