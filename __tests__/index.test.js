dfd33ec3aeafadd97621e999847c3a01d1a905ca
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-10-14 21:53:28 +0300

    Add "plain" formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,30 +10,40 @@ const cases = [
     getFixturePath('flat1.json'),
     getFixturePath('flat2.json'),
     getFixturePath('flat-expected.txt'),
+    'stylish',
   ],
   [
     getFixturePath('flat1.yml'),
     getFixturePath('flat2.yml'),
     getFixturePath('flat-expected.txt'),
+    'stylish',
   ],
   [
     getFixturePath('flat1.ini'),
     getFixturePath('flat2.ini'),
     getFixturePath('flat-expected.txt'),
+    'stylish',
   ],
   [
     getFixturePath('nested1.json'),
     getFixturePath('nested2.json'),
     getFixturePath('nested-expected.txt'),
+    'stylish',
+  ],
+  [
+    getFixturePath('nested1.json'),
+    getFixturePath('nested2.json'),
+    getFixturePath('nested-plain-expected.txt'),
+    'plain',
   ],
 ];
 
 describe('Should compare files', () => {
   test.each(cases)(
-    'Filepath1: %p, Filepath2: %p',
-    (filepath1, filepath2, filepath3) => {
+    'Filepath1: %p, Filepath2: %p, Expectedpath: %p, Format: %p',
+    (filepath1, filepath2, filepath3, format) => {
       const expected = readFile(filepath3);
-      const actual = genDiff(filepath1, filepath2, 'stylish');
+      const actual = genDiff(filepath1, filepath2, format);
 
       expect(actual).toEqual(expected);
     },
---
+++ src/formatters/plain.js
@@ -0,0 +1,58 @@
+import head from 'lodash/head.js';
+import tail from 'lodash/tail.js';
+import flattenDeep from 'lodash/flattenDeep.js';
+import isObject from 'lodash/isObject.js';
+import isString from 'lodash/isString.js';
+import find from 'lodash/find.js';
+import get from 'lodash/get.js';
+import identity from 'lodash/identity.js';
+
+const valueTypes = [
+  {
+    checker: (value) => isObject(value),
+    process: () => '[complex value]',
+  },
+  {
+    checker: (value) => isString(value),
+    process: (value) => `'${value}'`,
+  },
+];
+
+const toString = (value) => {
+  const item = find(valueTypes, ({ checker }) => checker(value));
+  const process = get(item, 'process', identity);
+
+  const result = process(value);
+
+  return result;
+};
+
+const genPath = (path, part) => `${path ? `${path}.${part}` : part}`;
+
+const nodeTypes = {
+  NESTED: (node, path, fn) => fn(node.value, [], genPath(path, node.key)),
+  ADDED: (node, path) => [`Property '${genPath(path, node.key)}' was added with value: ${toString(node.value)}`],
+  REMOVED: (node, path) => [`Property '${genPath(path, node.key)}' was removed`],
+  CHANGED: (node, path) => [`Property '${genPath(path, node.key)}' was updated. From ${toString(node.prevValue)} to ${toString(node.value)}`],
+  UNCHANGED: () => [],
+};
+
+const plain = (ast) => {
+  const iter = (tree, acc, path = '') => {
+    if (!tree.length) return flattenDeep(acc);
+
+    const node = head(tree);
+
+    const process = nodeTypes[node.type];
+
+    const result = process(node, path, iter);
+
+    return iter(tail(tree), [...acc, ...result], path);
+  };
+
+  const result = iter(ast, [], '');
+
+  return result.join('\n');
+};
+
+export default plain;
