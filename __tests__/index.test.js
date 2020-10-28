b05ff29cda111181a57f30a1114dd5f87060246a
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-10-28 20:18:49 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,58 +7,49 @@ const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
 
 const cases = [
   [
-    'before.json',
-    'after.json',
+    'json',
     'expected-stylish.txt',
     'stylish',
   ],
   [
-    'before.json',
-    'after.json',
+    'json',
     'expected-plain.txt',
     'plain',
   ],
   [
-    'before.json',
-    'after.json',
+    'json',
     'expected-json.txt',
     'json',
   ],
 
   [
-    'before.yml',
-    'after.yml',
+    'yml',
     'expected-stylish.txt',
     'stylish',
   ],
   [
-    'before.yml',
-    'after.yml',
+    'yml',
     'expected-plain.txt',
     'plain',
   ],
   [
-    'before.yml',
-    'after.yml',
+    'yml',
     'expected-json.txt',
     'json',
   ],
 
   [
-    'before.ini',
-    'after.ini',
+    'ini',
     'expected-stylish.txt',
     'stylish',
   ],
   [
-    'before.ini',
-    'after.ini',
+    'ini',
     'expected-plain.txt',
     'plain',
   ],
   [
-    'before.ini',
-    'after.ini',
+    'ini',
     'expected-json.txt',
     'json',
   ],
@@ -66,11 +57,11 @@ const cases = [
 
 describe('Should compare files', () => {
   test.each(cases)(
-    'Filepath1: %p, Filepath2: %p, Expectedpath: %p, Format: %p',
-    (filename1, filename2, filename3, format) => {
-      const filepath1 = getFixturePath(filename1);
-      const filepath2 = getFixturePath(filename2);
-      const filepath3 = getFixturePath(filename3);
+    'File extension: %p, Expectedpath: %p, Format: %p',
+    (extension, filename, format) => {
+      const filepath1 = getFixturePath(`before.${extension}`);
+      const filepath2 = getFixturePath(`after.${extension}`);
+      const filepath3 = getFixturePath(filename);
 
       const expected = readFile(filepath3);
       const actual = genDiff(filepath1, filepath2, format);
